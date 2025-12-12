9fa26495f4a7e49e5984031d7200c63a487bd0a3
komed3 <pk97@outlook.de>
2025-12-12 19:55:24 +0100

    add defaults

--- src/core/AreaController.ts
+++ src/core/AreaController.ts
@@ -3,10 +3,23 @@ import { ColorUtils } from './ColorUtils';
 
 export class AreaController extends DatasetController {
 
-    public static readonly id = 'area';
+    static readonly id = 'area';
+    static readonly defaults = {
+        datasetElementType: 'line',
+        dataElementType: 'point',
+        fill: 'origin',
+        tension: 0.1,
+        borderWidth: 2,
+        pointRadius: 3,
+        pointHoverRadius: 5,
+        threshold: 0,
+        fillOpacity: 0.6,
+        hoverState: false
+    };
 
     public initialize () : void {
         super.initialize();
+        const dataset = this.getDataset();
     }
 
 }
