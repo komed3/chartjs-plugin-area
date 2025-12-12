84b1504231be995888612b50eafca50f1da4c9db
komed3 <pk97@outlook.de>
2025-12-12 19:38:00 +0100

    parse color and convert to string

--- src/core/ColorUtils.ts
+++ src/core/ColorUtils.ts
@@ -1,1 +1,20 @@
-export class ColorUtils {}
+import { Color } from 'chart.js';
+
+export class ColorUtils {
+
+    public static toRGBA ( color: Color, alpha: number = 1 ) : string {
+        if ( typeof color === 'string' ) {
+            if ( color.startsWith( '#' ) ) {
+                const r = parseInt( color.slice( 1, 3 ), 16 );
+                const g = parseInt( color.slice( 3, 5 ), 16 );
+                const b = parseInt( color.slice( 5, 7 ), 16 );
+                return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+            } else if ( color.startsWith( 'rgb' ) ) {
+                return color.replace( /rgb\(([^)]+)\)/, `rgba($1, ${alpha})` );
+            }
+            return color;
+        }
+        return color.toString();
+    }
+
+}
