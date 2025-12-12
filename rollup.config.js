71d3b30fa5a174fdea8f6662a9dd4ed71f4689bf
komed3 <pk97@outlook.de>
2025-12-12 23:57:58 +0100

    rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,17 @@
 export default {
-    input: 'src/index.ts'
+    input: 'src/index.ts',
+    external: [ 'chart.js' ],
+    output: [ {
+        file: 'dist/index.js',
+        format: 'es',
+        sourcemap: true
+    }, {
+        file: 'dist/index.umd.js',
+        format: 'umd',
+        sourcemap: true,
+        name: 'AreaChartController',
+        globals: {
+            'chart.js': 'Chart'
+        }
+    } ]
 };
