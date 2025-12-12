0b608010650a030408646cfa0a0c7aafb4ebddb0
komed3 <pk97@outlook.de>
2025-12-12 19:30:53 +0100

    register area controller

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-import {} from 'chart.js';
+import { DatasetController, registry } from 'chart.js';
+
+export default class AreaController extends DatasetController {
+
+    public static readonly id = 'area';
+
+    public initialize () : void {
+        super.initialize();
+    }
+
+}
+
+registry.addControllers( AreaController );
